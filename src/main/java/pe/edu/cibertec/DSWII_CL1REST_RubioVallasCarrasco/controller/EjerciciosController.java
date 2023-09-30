68e872887a53e1c5ea8d6d79b21268867ca4cd41
jr256 <joser.72@outlook.com>
2023-09-30 14:21:38 -0500

    ejercicio prestamo

--- src/main/java/pe/edu/cibertec/DSWII_CL1REST_RubioVallasCarrasco/controller/EjerciciosController.java
+++ src/main/java/pe/edu/cibertec/DSWII_CL1REST_RubioVallasCarrasco/controller/EjerciciosController.java
@@ -1,11 +1,31 @@
 package pe.edu.cibertec.DSWII_CL1REST_RubioVallasCarrasco.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pe.edu.cibertec.DSWII_CL1REST_RubioVallasCarrasco.model.MensajeResponse;
+import pe.edu.cibertec.DSWII_CL1REST_RubioVallasCarrasco.service.EjerciciosService;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping(path = "api/v1/ejercicios")
 public class EjerciciosController {
+
+    private EjerciciosService ejerciciosService;
+
+
+    //Ejercicio préstamo
+    @GetMapping("/prestamo")
+    public ResponseEntity<MensajeResponse> cuotasPrestamo(@RequestParam double monto){
+
+        return new ResponseEntity<>(
+                ejerciciosService.calcularCuotas(monto), HttpStatus.OK);
+
+    }
+
+
 }
--- src/main/java/pe/edu/cibertec/DSWII_CL1REST_RubioVallasCarrasco/service/EjerciciosService.java
+++ src/main/java/pe/edu/cibertec/DSWII_CL1REST_RubioVallasCarrasco/service/EjerciciosService.java
@@ -1,7 +1,57 @@
 package pe.edu.cibertec.DSWII_CL1REST_RubioVallasCarrasco.service;
 
 import org.springframework.stereotype.Service;
+import pe.edu.cibertec.DSWII_CL1REST_RubioVallasCarrasco.model.MensajeResponse;
+
+import java.nio.DoubleBuffer;
+import java.text.DecimalFormat;
 
 @Service
 public class EjerciciosService {
+
+
+    // ******************Ejercicio prestamo************************
+    public MensajeResponse calcularCuotas(double prestamo){
+
+        if (prestamo < 1) {
+           return MensajeResponse.builder()
+                    .resultado("Monto de préstamo no puede ser menor a 1 ")
+                    .build();
+        }
+
+        double interes = (prestamo < 4000) ? 0.12 : 0.10;
+        double totalDeuda;
+        int cuotas;
+
+        if (prestamo > 5000) {
+            totalDeuda = prestamo * (1 + interes);
+            cuotas = 3;
+        } else if (prestamo < 1000) {
+            totalDeuda = prestamo * (1 + interes);
+            cuotas = 1;
+        } else if (prestamo >= 2000 && prestamo <= 3000) {
+            totalDeuda = prestamo * (1 + interes);
+            cuotas = 2;
+        } else {
+            totalDeuda = prestamo * (1 + interes);
+            cuotas = 5;
+        }
+
+        double montoCuota = totalDeuda / cuotas;
+        double tasaInteres = interes * 100;
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        double montoCuotaDF = Double.parseDouble(df.format(montoCuota));
+        double totalDeudaDF = Double.parseDouble(df.format(totalDeuda));
+
+        return MensajeResponse.builder()
+                .resultado(
+                        "Cantidad cuotas: " + cuotas +
+                        " , Monto cuota: " + montoCuotaDF +
+                        " , Total deuda: " + totalDeudaDF +
+                        " , Tasa de interés: " + tasaInteres+"%"
+
+                )
+                .build();
+    }
 }
